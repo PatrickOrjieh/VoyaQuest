f0e6c2bc0c7078054cb7ba400509e540afd98fc0
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-17 16:45:45 +0100

    Add the currency conversion interface

---
+++ Enums/Currency.cs
@@ -0,0 +1,12 @@
+﻿namespace VoyaQuest.Enums
+{
+    public enum Currency
+    {
+        USD, // United States Dollar
+        EUR, // Euro
+        GBP, // British Pound
+        JPY, // Japanese Yen
+        CAD, // Canadian Dollar
+        AUD  // Australian Dollar
+    }
+}
---
+++ Interfaces/ICurrencyService.cs
@@ -0,0 +1,24 @@
+﻿namespace VoyaQuest.Interfaces
+{
+    /// <summary>
+    /// This interface defines the methods for currency conversion.
+    /// </summary>
+    public interface ICurrencyService
+    {
+        /// <summary>
+        /// This method retrieves the exchange rate between two currencies.
+        /// </summary>
+        /// <param name="fromCurrency">The currency to convert from.</param>
+        /// <param name="toCurrency">The currency to convert to.</param>
+        /// <returns>Returns the exchange rate between the two currencies.</returns>
+        Task<decimal> GetExchangeRateAsync(string fromCurrency, string toCurrency);
+        /// <summary>
+        /// This method converts an amount from one currency to another.
+        /// </summary>
+        /// <param name="amount">The amount to convert.</param>
+        /// <param name="fromCurrency">the currency to convert from.</param>
+        /// <param name="toCurrency">The currency to convert to.</param>
+        /// <returns>Returns the converted amount.</returns>
+        Task<decimal> ConvertAmountAsync(decimal amount, string fromCurrency, string toCurrency);
+    }
+}
