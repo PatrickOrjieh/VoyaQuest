40f4342ebafe2d1f4ddfb33abfdbe94dad8be639
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-17 16:50:22 +0100

    Add the models for the currency response deserialization

---
+++ Models/CurrencyResponse/Rootobject.cs
@@ -0,0 +1,11 @@
+﻿namespace VoyaQuest.Models.CurrencyResponse
+{
+    public class Rootobject
+    {
+        public string disclaimer { get; set; }
+        public string license { get; set; }
+        public int timestamp { get; set; }
+        public string @base { get; set; }
+        public Rates rates { get; set; }
+    }
+}
